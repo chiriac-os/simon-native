c4f576ee0514645b5e201ca56812ccb1eb9f542b
Chiriac <andrei-chiriac@outlook.com>
2023-12-07 18:31:49 +0100

    Mod: complete board

--- src/components/buttons/ColoredButton.jsx
+++ src/components/buttons/ColoredButton.jsx
@@ -1,25 +1,33 @@
+import React, { useEffect } from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 
-function ColoredButton({ color, ref, onPress }) {
+/**
+ * @param {{string, function}} props
+ * @param {React.Ref} ref
+ * @returns {React.ForwardRefExoticComponent<React.RefAttributes<React.JSX.Element>>}
+ */
+const ColoredButton = React.forwardRef((props, ref) => {
+    const { color, onPress } = props;
+    
     return (
         <View style={styles().view}>
             <TouchableOpacity ref={ref} style={styles(color).button} onPress={onPress} />
         </View>
     );
-}
+});
 
 const styles = (color) => StyleSheet.create({
     view: {
         borderWidth: 10,
         borderColor: "#000",
         borderRadius: 20,
-        margin: 25,
+        margin: 10,
     },
     button: {
         backgroundColor: color,
-        height: 200,
-        width: 200,
-        borderRadius: 20,
+        height: 150,
+        width: 150,
+        borderRadius: 10,
     },
 });
 
--- src/components/game/Board.jsx
+++ src/components/game/Board.jsx
@@ -1,10 +1,29 @@
-import { View, StyleSheet} from "react-native";
+import { useState, useRef } from "react";
+import { View, StyleSheet } from "react-native";
 import ColoredButton from "../buttons/ColoredButton";
 
 function Board() {
+    /**
+     * Hooks
+     */
+    const [gamePattern, setGamePattern] = useState([]);
+    const red = useRef();
+    const blue = useRef();
+    const green = useRef();
+    const yellow = useRef();
+
+    const seeRef = (ref) => { console.log(ref) }
+
     return (
         <View style={styles.view}>
-            <ColoredButton color="red" />
+            <View style={styles.row}>
+                <ColoredButton ref={green} color="green" onPress={() => seeRef(green)} />
+                <ColoredButton ref={red} color="red" onPress={() => seeRef(red)} />
+            </View>
+            <View style={styles.row}>
+                <ColoredButton ref={yellow} color="yellow" onPress={() => seeRef(yellow)} />
+                <ColoredButton ref={blue} color="blue" onPress={() => seeRef(blue)} />
+            </View>
         </View>
     )
 }
@@ -13,6 +32,9 @@ const styles = StyleSheet.create({
     view: {
 
     },
+    row: {
+        flexDirection: "row",
+    }
 });
 
 export default Board;
